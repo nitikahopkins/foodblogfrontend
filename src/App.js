d148143014cd95f7067612a02793d6d4d97594f7
Nitika Chaudhary <nitika.hopkins@gmail.com>
2020-06-29 19:52:12 -0400

    added useState, sign-in, removed withAutenticator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Auth } from "aws-amplify";
 import "./App.css";
-import { withAuthenticator } from "@aws-amplify/ui-react";
-
-async function signOut() {
-  try {
-    await Auth.signOut({ global: true }).then(() => {
-      window.location = "/cats";
-    });
-  } catch (error) {
-    console.log(error);
-  }
-}
 
 function App() {
+  const [signedInUser, setSignedInUser] = useState(undefined);
+  const [signInForm, setSignInForm] = useState({ username: "", password: "" });
+
+  async function signOut() {
+    try {
+      await Auth.signOut({ global: true }).then(() => {
+        window.location = "";
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function signIn() {
+    try {
+      console.log(signInForm);
+      const user = await Auth.signIn(signInForm.username, signInForm.password);
+      setSignedInUser(user);
+      console.log(await Auth.currentAuthenticatedUser());
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // useEffect(() => {
+  //   (async () => console.log(await Auth.currentAuthenticatedUser()))();
+  // }, []);
   return (
     <div className="App">
       <button onClick={signOut}>Logout</button>
+      <div>
+        <input
+          onChange={(e) =>
+            setSignInForm({ ...signInForm, username: e.target.value })
+          }
+        />
+        <input
+          type="password"
+          onChange={(e) =>
+            setSignInForm({ ...signInForm, password: e.target.value })
+          }
+        />
+        <button onClick={signIn}>Sign In</button>
+      </div>
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
