0b4b723de9758bfa06c1c5f2c984bc0b234e82b0
Nitika Chaudhary <nitika.hopkins@gmail.com>
2020-06-29 19:25:20 -0400

    added sign-in and logout (hard coded the path - not recommended)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Auth } from "aws-amplify";
+import "./App.css";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+
+async function signOut() {
+  try {
+    await Auth.signOut({ global: true }).then(() => {
+      window.location = "/cats";
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={signOut}>Logout</button>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
